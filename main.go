0e16ea3149cb418be9c18069009b1951842482c1
da-mckeel <damckeel13@gmail.com>
2026-03-29 21:30:57 -0500

    Updated main with a REPL that returns the first word typed in lowercase

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex >")
+		scanner.Scan()
+		text := scanner.Text()
+		firstWord := cleanInput(text)[0]
+		fmt.Printf("Your command was: %s\n", firstWord)
+	}
 }
