e214b8f88b8b42f02b6daee8fe44ea24f05de307
da-mckeel <damckeel13@gmail.com>
2026-03-30 20:20:18 -0500

    Removed print first word logic. Added exit and help commands.

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex >")
-		scanner.Scan()
-		text := scanner.Text()
-		firstWord := cleanInput(text)[0]
-		fmt.Printf("Your command was: %s\n", firstWord)
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,9 +1,50 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+	return commands
+}
+
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex >")
+		scanner.Scan()
+		text := scanner.Text()
+		command, exists := getCommands()[cleanInput(text)[0]]
+		if !exists {
+			fmt.Println("Unknown command")
+			continue
+		}
+		command.callback()
+	}
 }
